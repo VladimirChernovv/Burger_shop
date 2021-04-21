4e72c15a0d65616dd0bd16954e4f94fa3e53c239
Vladimir Chernov <v.chernov@zebrains.team>
2021-04-21 19:21:19 +0300

    Создали автаризацию на приложении через github(подтягиваем от туда фото-аватар). Сделали настройку базы данных сервиса firebase - установили правила для firebase-rules

--- src/base.js
+++ src/base.js
@@ -1,6 +1,7 @@
 import Rebase from 're-base';
 import firebase from 'firebase/app';
-require('firebase/database');
+import 'firebase/database';
+import 'firebase/auth';
 
 const firebaseApp = firebase.initializeApp({
   apiKey: "AIzaSyB3lgl5WRNH2j4Iqt7T_9bhQYgLYE1dKSM",
